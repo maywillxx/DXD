9c0147479c3ab144846a2b54e55679e1efb27e3f
maywill <1041658941@qq.com>
2018-08-07 17:19:14 +0800

    20180807 17.19

---
+++ ConsoleApplication2/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //兔子繁殖问题。设有一对新生的兔子，从第三个月开始他们每个月都生一对兔子，新生的兔子从第三个月开始又每个月生一对兔子。按此规律，并假定兔子没有死亡，20个月后共有多少个兔子？
+            int sum = 0;
+            for (int m = 1; m <= 20; m++)
+            {
+                if (m >= 3)
+                {
+                    sum++;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //求出1~1000之间的所有能被7整除的数，并计算和输出每5个的和。
+            int sum = 0;
+            int count = 0;
+            for (int num = 1; num <=1000; num++)
+            {
+                if (num%7==0)
+                {
+                    count++;
+                    sum += num;
+                    if (count == 5)
+                    {
+                        Console.WriteLine(sum);
+                        count = 0;
+                        sum = 0;
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
